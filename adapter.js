f2593373715dfd8525a1c14fd3f0722aa8399f35
Morgan Croney <m@morgancroney.com>
2015-11-24 15:57:45 -0500

    Throw error if html not preprocessed.

--- adapter.js
+++ adapter.js
@@ -1,10 +1,12 @@
 (function(window) {
 
   window.nunjucks.Environment.prototype.getPreprocessedTemplate = function (path) {
-    if (window.__html__[path]) {
-      return nunjucks.compile(window.__html__[path], this);
-    } else {
+    if (!window.__html__) {
+      throw new Error('html2js preprocessor is required for template files');
+    } else if (!window.__html__[path]) {
       throw new Error('nunjucks template does not exist');
+    } else {
+      return nunjucks.compile(window.__html__[path], this);
     }
   };
 
